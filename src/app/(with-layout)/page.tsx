fb509708485a17bcbf1d408db7e2b92b5c847de6
John Doe <mohaiminur24@gmail.com>
2023-07-21 17:23:34 +0600

    responsive footer design has complete

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,10 +1,14 @@
+import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 
-export default function Home() {
+export default function Home({children}:{children:string}) {
   return (
     <>
     <Header/>
-    
+    <div className="w-11/12 mx-auto min-h-[calc(100vh-200px)]">
+    {children}
+    </div>
+    <Footer/>
     </>
   );
 }
---
+++ src/app/Components/Footer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 bg-base-200 text-base-content">
+        <div>
+          <h1 className="text-3xl tracking-tighter font-bold text-yellow-500">GadgetNest</h1>
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
